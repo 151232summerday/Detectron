a6fcf6459f2b700160f427a8de990f5fcdc3168a
linkaixiang_i <linkaixiang_i@didichuxing.com>
2018-08-02 09:30:52 +0800

    test

--- tools/infer_simple_test.py
+++ tools/infer_simple_test.py
@@ -2,3 +2,39 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
+
+from collections import defaultdict
+import argparse
+import cv2  # NOQA (Must import before importing caffe2 due to bug in cv2)
+import glob
+import logging
+import os
+import sys
+import time
+
+from caffe2.python import workspace
+import sys
+sys.path.append("..")
+import pyximport
+pyximport.install()
+from detectron.core.config import assert_and_infer_cfg
+from detectron.core.config import cfg
+from detectron.core.config import merge_cfg_from_file
+from detectron.utils.io import cache_url
+from detectron.utils.logging import setup_logging
+from detectron.utils.timer import Timer
+import detectron.core.test_engine as infer_engine
+import detectron.datasets.dummy_datasets as dummy_datasets
+import detectron.utils.c2 as c2_utils
+import detectron.utils.vis as vis_utils
+
+c2_utils.import_detectron_ops()
+cv2.ocl.setUseOpenCL(False)
+
+
+
+
+print (111)
+
+
+
